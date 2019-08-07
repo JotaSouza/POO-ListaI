c91ea832918d6ff5456a12ee3f3ba3b4b15883c9
unknown <cge.sede@IFC-CGE03.IFRSL.local>
2019-08-07 13:11:49 -0300

    Create MetrosEmCm.java

---
+++ MetrosEmCm.java
@@ -0,0 +1,16 @@
+package Lista1;
+
+public class MetrosEmCm {
+
+	public static void main(String[] args) {
+		
+		float metros, centimetros;
+		
+		metros = 12;
+		centimetros = metros * 100f;
+		
+		System.out.println(centimetros);
+
+	}
+
+}
