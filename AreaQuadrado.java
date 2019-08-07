b9d412654a96e7c7a62e27df15453d8e56bb7d84
unknown <cge.sede@IFC-CGE03.IFRSL.local>
2019-08-07 13:11:35 -0300

    Create AreaQuadrado.java

---
+++ AreaQuadrado.java
@@ -0,0 +1,19 @@
+package Lista1;
+
+public class AreaQuadrado {
+
+	public static void main(String[] args) {
+		
+		int base, altura, area;
+		
+		base = 3;
+		altura = base;
+		area = base*altura;
+		
+		System.out.println(area);
+		
+		
+
+	}
+
+}
